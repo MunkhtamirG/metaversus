3c32ea7705c5acdea944f01c15f75872faa1b557
MBANK\munkhtamir.g <munkhtamir.g@m-bank.mn>
2023-01-17 11:17:41 +0800

    MOD

--- pages/_app.js
+++ pages/_app.js
@@ -5,13 +5,16 @@ const MyApp = ({ Components, pageProps }) => (
   <>
     <Head>
       <title>Metaversus</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
       <link rel="preconnect" href="https://stijndv.com" />
       <link
         rel="stylesheet"
         href="https://stijndv.com/fonts/Eudoxus-Sans.css"
       />
-      <link rel="icon" href="/favicon.ico" />
     </Head>
     <Components {...pageProps} />
   </>
 );
+
+export default MyApp;
